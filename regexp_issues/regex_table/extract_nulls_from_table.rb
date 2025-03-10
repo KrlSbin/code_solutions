ecb3d8f26005f0d60f9a3a978dc8db4b7c04ecd9
Kyrylo Skriabin <ckp9l6ih@gmail.com>
2025-03-10 16:47:03 +0200

    Change regexp in regex_table issue

--- regexp_issues/regex_table/extract_nulls_from_table.rb
+++ regexp_issues/regex_table/extract_nulls_from_table.rb
@@ -1,5 +1,6 @@
 class ExtractNullsFromTable
-  EXTRACT_NULLS_REGEXP = Regexp.new('(?<=\\n)NULL,[^\\n]*\\n|(?<=\\n)[^\\n]*,NULL,[^\\n]*\\n|(?<=\\n)[^\\n]*,NULL\\n')
+  # EXTRACT_NULLS_REGEXP = Regexp.new('(?<=\\n)NULL,[^\\n]*\\n|(?<=\\n)[^\\n]*,NULL,[^\\n]*\\n|(?<=\\n)[^\\n]*,NULL\\n')
+  EXTRACT_NULLS_REGEXP = Regexp.new('^.*\bNULL\b.*\\n')
   class << self
     def do(file)
       file.gsub(EXTRACT_NULLS_REGEXP, '')
